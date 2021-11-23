a137bd552224225cc7ada9113f9c20311dd88812
Lennert <99069053@mydavinci.nl>
2021-11-23 09:41:14 +0100

    Multiplier.py

---
+++ multiplier-x.py/multiplier-x.py
@@ -0,0 +1,9 @@
+# multiplier-x.py
+
+def multiplier(getal:int):
+    for i in range(1,11):
+        print(f'{nummer} x {i} = ' + str(nummer*i))
+
+nummer = int(input('Van welk cijfer wil je de tafel zien? '))
+
+multiplier(nummer)
